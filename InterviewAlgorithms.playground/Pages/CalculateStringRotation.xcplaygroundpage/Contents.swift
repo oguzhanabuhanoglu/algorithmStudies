c4308d397e93d5df75cb22e91119516c2741748b
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-05-23 00:29:06 +0300

    SortedBySecondCharacter

--- InterviewAlgorithms.playground/Pages/CalculateStringRotation.xcplaygroundpage/Contents.swift
+++ InterviewAlgorithms.playground/Pages/CalculateStringRotation.xcplaygroundpage/Contents.swift
@@ -19,4 +19,4 @@ func findRotationIndex(_ str1: String, _ str2: String) -> Int {
     return -1
 }
 
-findRotationIndex("eecoff", "coffee")
+findRotationIndex("Mouse", "mouse")
---
+++ InterviewAlgorithms.playground/Pages/SortedBySecondCharacter.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ bir string dizisi verildiğinde, diziyi stringlerin ikinci karakterine göre artan düzende sıralayan algoritmayı yazınız.     ÖRNEK: Input = ["b1", "c2", "x3", "x4", "z0"]. Output = ["z0", "b1", "c2", "x3", "x4"]
+ */
+func sortBySecondCharacter(_ array: [String]) -> [String] {
+    return array.sorted { first, second in
+        
+        let firstIndex = first.index(first.startIndex, offsetBy: 1)
+        let secondIndex = second.index(second.startIndex, offsetBy: 1)
+        
+        return first[firstIndex] < second[secondIndex]
+    }
+}
+
+// Örnek kullanım
+let input = ["b1", "c2", "x3", "x4", "z0"]
+let output = sortBySecondCharacter(input)
+print(output) // Çıktı: ["z0", "b1", "c2", "x3", "x4"]
